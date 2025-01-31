6adf491aee957bc1bafa0cad1106f577bc2ca76c
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2025-01-31 13:40:11 -0600

    Refactor getRandomWords function to combine words from multiple quotes and improve variable naming for clarity

--- messageGenerator.js
+++ messageGenerator.js
@@ -19,29 +19,43 @@ function generateQuote() {
   return quotes[randomIndex];
 }
 
-function getFirstWord(message) {
-  const firstWord = message.split(" ")[0];
+function getFirstWord(quote) {
+  const firstWord = quote.split(" ")[0];
   return firstWord;
 }
 
-function getRandomWords(message, numberOfWords) {
-  const cleanedMessage = message.replace(/[.-]/g, "");
-  const words = cleanedMessage.split(" ");
+function getRandomWords(quote, numberOfWords) {
+  const allWords = [];
+
+  // Combine words from all quotes
+  quotes.forEach((quote) => {
+    const cleanedQuote = quote.replace(/[.-]/g, "");
+    const words = cleanedQuote.split(" ");
+    words.forEach((word) => {
+		allWords.push(word.trim().toLowerCase());
+  });
+  });
+
   const randomWords = [];
 
   for (let i = 0; i < numberOfWords; i++) {
-    const randomIndex = getRandomInt(words.length);
-    randomWords.push(words[randomIndex].trim().toLowerCase());
+    const randomIndex = getRandomInt(allWords.length);
+    randomWords.push(allWords[randomIndex]);
   }
 
   return randomWords.join(" ");
 }
 
-const randomQuote = generateQuote();
-const firstWord = getFirstWord(randomQuote);
-const shuffledQuote =
-  firstWord + " " + getRandomWords(generateQuote(), 4) + ".";
+const randomQuote1 = generateQuote();
+const randomQuote2 = generateQuote();
+const randomQuote3 = generateQuote();
+const firstWord = getFirstWord(randomQuote1);
 
-console.log(randomQuote);
+const shuffledQuote =
+  firstWord + ' ' + getRandomWords([randomQuote1, randomQuote2, randomQuote3], 4) + ".";
+
+console.log(randomQuote1);
+console.log(randomQuote2);
+console.log(randomQuote3);
 console.log(`The first word of the quote: ${firstWord}`);
 console.log(`New random quote: ${shuffledQuote}`);
